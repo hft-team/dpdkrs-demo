da900f257fcf5f5c2cbffa17ca944526fbffa4e1
root <root@ip-172-31-12-142.ap-northeast-1.compute.internal>
2023-02-22 15:57:27 +0800

    bench

--- README.md
+++ README.md
@@ -1,6 +1,33 @@
 
 
 
-RUST_LOG=TRACE ./target/debug/dpdkrs-demo --conf config.ini  --proc-type=primary --proc-id=0
+cargo run --bin client_reqwest
 
-RUST_LOG=TRACE ./target/debug/client --conf config.ini  --proc-type=primary --proc-id=0
+## 测试记录
+
+## 第一次试验(100ms 请求一次,请求100次，进行json解析)
+ave cost = 748.057µs
+ave cost = 776.064µs
+ave cost = 826.657µs
+ave cost = 825.99µs
+ave cost = 798.619µs
+
+794.6us
+
+## 第二次试验(连续请求100次，进行json解析)
+ave cost = 423.065µs
+ave cost = 423.485µs
+ave cost = 490.671µs
+ave cost = 512.955µs
+ave cost = 430.626µs
+
+455.6us
+
+## 第三次试验(连续请求100次，不进行json解析)
+ave cost = 439.275µs
+ave cost = 408.595µs
+ave cost = 369.347µs
+ave cost = 390.571µs
+ave cost = 441.685µs
+
+409us
--- src/client_reqwest.rs
+++ src/client_reqwest.rs
@@ -1,13 +1,36 @@
 #[tokio::main]
 async fn main() -> Result<(), reqwest::Error> {
     dpdk_io::bootstrap();
-    let echo_json: serde_json::Value = reqwest::Client::new()
-        .get("http://172.31.10.131/ping")
-        .send()
-        .await
-        .expect("send")
-        .json()
-        .await?;
-    println!("{:#?}", echo_json);
+    time_analyse().await
+}
+
+async fn time_analyse() -> Result<(), reqwest::Error> {
+    let client = reqwest::Client::new();
+    let mut time_cost = vec![];
+    for _ in 0..100 {
+        // tokio::time::sleep(std::time::Duration::from_millis(100)).await;
+        // println!("start to curl");
+        let start = std::time::Instant::now();
+        let _json_body = client.get("http://172.31.10.131/ping").send().await?;
+        // .expect("send")
+        // .json()
+        // .await?;
+        // println!("{:#?}", json_body);
+
+        let cost = start.elapsed();
+        time_cost.push(cost);
+    }
+
+    println!("{:#?}", time_cost);
+    let mut sum = std::time::Duration::ZERO;
+    for (i, c) in time_cost.iter().enumerate() {
+        if i == 0 {
+            continue;
+        }
+        sum += *c;
+    }
+
+    println!("ave cost = {:?}", sum / 99);
+
     Ok(())
 }
